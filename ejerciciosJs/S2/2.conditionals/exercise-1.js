d4c93913f0fea5978dc86cb8dbe1e01003a2b483
luiki22 <lsaenzrivero@gmail.com>
2023-10-13 08:44:31 +0200

    Nueva actualización

--- ejerciciosJs/S2/2.conditionals/exercise-1.js
+++ ejerciciosJs/S2/2.conditionals/exercise-1.js
@@ -4,5 +4,35 @@ const alumns = [
     {name: 'Abel Cabeza', T1: false, T2: true, T3: true}, 
     {name: 'Alfredo Blanco', T1: false, T2: false, T3: false}, 
     {name: 'Raquel Benito', T1: true, T2: true, T3: true}
-]
+];
 
+
+for( let i = 0; i < alumns.length; i++){
+        
+    if (alumns[i].T1 === false && alumns[i].T2 === false ){
+        
+        
+        console.log(alumns[i].name, ': ', 'No isApproved');
+    } 
+
+    else if(alumns[i].T1 === true && alumns[i].T2 === false && alumns[i].T3 === true){
+        
+        
+        console.log(alumns[i].name, ': ', 'isApproved');
+    }
+
+    else if(alumns[i].T1 === false && alumns[i].T2 === true && alumns[i].T3 === true){
+        
+        
+        console.log(alumns[i].name, ': ', 'isApproved');
+    }
+    
+    if (alumns[i].T1 === true && alumns[i].T2 === true ){
+        
+        
+        console.log(alumns[i].name, ': ', 'isApproved');
+    } 
+
+
+}
+    
---
+++ ejerciciosJs/S2/extra/exercise-1.js
@@ -0,0 +1,16 @@
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+ 
+for( categories of movies){
+    let newArray = categories.categories;
+    console.log(newArray);
+}
+
+
+
+
